dd90887f3a7cda04fa2da4f54f1a48cdc7dacdb1
Proofexak <proofexak@gmail.com>
2017-05-24 19:10:09 +0200

    Even better look

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   get 'home/about'
-  get 'devise/new'
+  get 'users/sign_up'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
